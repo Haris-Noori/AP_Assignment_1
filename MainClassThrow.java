4f66a09de6ffef6ec120a8c6566af77955b140a8
Haris-Noori <harisnoori20@gmail.com>
2021-04-08 20:49:57 +0500

    First commit

---
+++ MainClassThrow.java
@@ -0,0 +1,13 @@
+public class MainClassThrow{
+
+    public static void fn() throws ArithmeticException, ArrayIndexOutOfBoundsException
+    {
+        int array[] = {1,2,3};
+        System.out.println("Division = " + (10/5));
+        System.out.println(array[4]); 
+    }
+
+    public static void main(String[] args){
+        fn();
+    }
+}
